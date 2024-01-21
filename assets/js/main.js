87a11d35fbfe3f407073a3e5942dab5cc51e3b29
TahmidCynha <tahmidhossain2042@gmail.com>
2024-01-21 22:54:30 +0600

    version-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,15 +201,26 @@ const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
-// *TOP 
-e(".scrollToTop").each((function() {
-  e(this).on("click", (function(t) {
-      return t.preventDefault(),
-      e("html, body").animate({
-          scrollTop: 0
-      }, s / 3),
-      !1
+// *PROGRESS BAR START
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
   }
-  ))
-}
-))
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient( #FF6F61 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+// *PROGRESS BAR END 
